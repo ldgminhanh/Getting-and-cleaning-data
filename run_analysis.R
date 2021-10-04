f66c9ebe5b8eadb498e7060e90c79353d6e035ad
ldgminhanh <88870630+ldgminhanh@users.noreply.github.com>
2021-10-04 11:50:36 +0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,9 @@
-#Load and merge two data (test and train data)
+# Load and merge two data (test and train data)
 testdata <- read.table("C:/Users/kimmi/Desktop/UCI HAR Dataset/test/X_test.txt", header = F)
 traindata <- read.table("C:/Users/kimmi/Desktop/UCI HAR Dataset/train/X_train.txt", header = F)
 bigdata <- rbind(testdata, traindata)
-#Add column's name, select columns have "mean" and "std"
+
+# Add column's name, select columns have "mean" and "std"
 feature <- read.table("C:/Users/kimmi/Desktop/UCI HAR Dataset/features.txt")
 newfeature <- as.data.frame(t(feature))
 names = newfeature[2,]
@@ -12,12 +13,14 @@ meandata <- bigdata[, c(colmean)]
 colstd <- grep("std()", colnames(bigdata))
 stddata <- bigdata[, c(colstd)]
 mean_stddata1 <- cbind(meandata, stddata)
-#Add subject column
+
+# Add subject column
 sub_test <- read.table("C:/Users/kimmi/Desktop/UCI HAR Dataset/test/subject_test.txt")
 sub_train <- read.table("C:/Users/kimmi/Desktop/UCI HAR Dataset/train/subject_train.txt")
 subject <- rbind(sub_test, sub_train)
 mean_stddata2 <- cbind(subject, mean_stddata1)
-#Match activity labels
+
+# Match activity labels
 Y_test <- read.table("C:/Users/kimmi/Desktop/UCI HAR Dataset/test/y_test.txt")
 data1 <- replace(Y_test, Y_test == 1, "WALKING")
 data2 <- replace(data1, data1 == 2, "WALKING_UPSTAIRS")
@@ -34,11 +37,14 @@ data10 <- replace(data9, data9 == 5, "STANDING")
 Y_trainactlabels <- replace(data10, data10 == 6, "LAYING")
 actlabels <- rbind(Y_testactlabels, Y_trainactlabels)
 mean_stddata3 <- cbind(actlabels, mean_stddata2)
-#Rename column 1 and 2
+
+# Rename column 1 and 2
 mean_stddata3<-rename(mean_stddata3, c('activities' = 'V1', 'subject' = 'V1'))
 mean_stddata3<-rename(mean_stddata3, c('activities' = 'subject'))
 mean_stddata3<-rename(mean_stddata3, c('subject' = 'V1'))
-#Return tidy data with the average of each variable for each activity and each subject
+
+# Return tidy data with the average of each variable for each activity and each subject
 Finaldata <- mean_stddata3 %>% group_by(activities, subject) %>% summarise_all(mean)
+write.table(Finaldata, "C:/Users/kimmi/Desktop/GGetting-and-cleaning-data/")
 
 
